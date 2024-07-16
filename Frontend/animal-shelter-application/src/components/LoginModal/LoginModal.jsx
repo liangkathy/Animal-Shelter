bb4f9ba5cc051795aceb3793d9529bf29dcbabbd
liangkathy <jliang@indeed.com>
2024-07-16 16:37:07 -0500

    added error message class names to add pet modal, fixed auth navigation from login modal to land on log in or sign up based on user click

--- Frontend/animal-shelter-application/src/components/LoginModal/LoginModal.jsx
+++ Frontend/animal-shelter-application/src/components/LoginModal/LoginModal.jsx
@@ -15,17 +15,17 @@ const LoginModal = ({closeLoginModal, setIsLogin}) => {
 
     const setLogIn = () => {
       setIsLogin(true)
-  }
+    }
 
-  return (
-    <div className={`modal-container modal-container-${theme}`}>
-        <div id="login-modal" className={`modal-${theme}`}>
-                <IoClose onClick={closeLoginModal} className='close-modal' size="1.5em"/>
-                <h3>This feature requires an account</h3>
-                <div>Please <Link to="/auth" onClick={setLogIn}>log in</Link> or <Link to='/auth' onClick={setSignUp}>sign up</Link></div>
-        </div>
-    </div>
-  )
+    return (
+      <div className={`modal-container modal-container-${theme}`}>
+          <div id="login-modal" className={`modal-${theme}`}>
+                  <IoClose onClick={closeLoginModal} className='close-modal' size="1.5em"/>
+                  <h3>This feature requires an account</h3>
+                  <div>Please <Link to="/auth" onClick={setLogIn}>log in</Link> or <Link to='/auth' onClick={setSignUp}>sign up</Link></div>
+          </div>
+      </div>
+    )
 }
 
 export default LoginModal
